2064f86d2f26d1ad431179239890def7b36531e1
Lars Assen (student) <s1102416@student.windesheim.nl>
2021-06-01 21:37:01 +0200

    added some code for the database layer

---
+++ ReversiRestApi/DAL/SpelAccessLayer.cs
@@ -0,0 +1,29 @@
+﻿using ReversiMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReversiRestApi.DAL
+{
+	public class SpelAccessLayer : ISpelRepository
+	{
+		public void AddSpel(Spel.Spel spel)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Spel.Spel GetSpel(string spelToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Spel.Spel> GetSpellen()
+		{
+			var spelList = new List<Spel.Spel>();
+
+			string sqlQuery = "";
+			return spelList;
+		}
+	}
+}
