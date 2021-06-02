22adacb8d1148119638f200d3660bee9fa54241f
Lars Assen <juniorassen@hotmail.com>
2021-06-02 15:58:33 +0200

    added the getspellen function to the DAL

--- ReversiRestApi/DAL/SpelAccessLayer.cs
+++ ReversiRestApi/DAL/SpelAccessLayer.cs
@@ -1,6 +1,7 @@
 ﻿using ReversiMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace ReversiRestApi.DAL
 {
 	public class SpelAccessLayer : ISpelRepository
 	{
+		private const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ReversiDbRestApi;Integrated Security=True;";
 		public void AddSpel(Spel.Spel spel)
 		{
 			throw new NotImplementedException();
@@ -22,7 +24,28 @@ namespace ReversiRestApi.DAL
 		{
 			var spelList = new List<Spel.Spel>();
 
-			string sqlQuery = "";
+			string sqlQuery = "SELECT * FROM Game";
+
+			using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+			{
+				sqlCon.Open();
+				SqlCommand sqlCmd = new SqlCommand(sqlQuery, sqlCon);
+				SqlDataReader rdr = sqlCmd.ExecuteReader();
+
+				while (rdr.Read())
+				{
+					var spel = new Spel.Spel();
+					spel.ID = Convert.ToInt32(rdr["GUID"]);
+					spel.Omschrijving = rdr["Omschrijving"].ToString();
+					spel.Speler1Token = rdr["Speler1Token"].ToString();
+					spel.Speler2Token = rdr["Speler2Token"].ToString();
+					//TODO Convert this string into a gameboard
+					var gameState = rdr["GameState"].ToString();
+					spelList.Add(spel);
+				}
+				sqlCon.Close();
+			}
+
 			return spelList;
 		}
 	}
