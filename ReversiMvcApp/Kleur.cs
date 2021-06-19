d5fd0e14b4a5f4ebf034f087ca34e2688da6f88d
Lars Assen <juniorassen@hotmail.com>
2021-06-19 17:02:11 +0200

    moved spel classes into 1 model class

---
+++ ReversiMvcApp/Kleur.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReversiMvcApp
+{
+	public enum Kleur { Geen, Wit, Zwart };
+}
--- ReversiRestApi/Services/SpelRepository.cs
+++ ReversiRestApi/Services/SpelRepository.cs
@@ -1,4 +1,5 @@
-﻿using ReversiRestApi;
+﻿using ReversiMvcApp;
+using ReversiRestApi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
